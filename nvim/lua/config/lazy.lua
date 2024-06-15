caf72bddde8161f01751e298b570964a2a36c6cf
uumkon <uumkon@gmail.com>
2024-06-15 11:44:13 -0700

    update

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -17,7 +17,7 @@ require("lazy").setup({
       "LazyVim/LazyVim",
       import = "lazyvim.plugins",
       opts = {
-        colorscheme = "gruvbox",
+        colorscheme = "solarized-osaka",
         news = {
           lazyvim = true,
           neovim = true,
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,6 @@
 return {
   {
-    "ellisonleao/gruvbox.nvim",
+    "craftzdog/solarized-osaka.nvim",
     lazy = true,
     priority = 1000,
     opts = function()

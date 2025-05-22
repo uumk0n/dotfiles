57de5f00d02c732c71d1a38e2b8a32b696502fda
Илья Коняев <ilakonaev@MacBook-Air-Ila.local>
2025-05-22 15:18:26 +0500

    add aerospace, sketchybar, zen and other

--- setup_macos.sh
+++ setup_macos.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#Tools
 brew install bat yazi postgresql fzf tree npm fd nvim tmux lazygit ripgrep
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
@@ -8,20 +9,36 @@ git clone https://github.com/LazyVim/starter ~/.config/nvim
 
 rm -rf ~/.config/nvim/.git
 
+# configs
 cp -r ./nvim/* ~/.config/nvim/
 
 mkdir ~/.config/ghostty/
 mkdir ~/.config/yazi/
+mkdir ~/.config/sketchybar/
+mkdir ~/.config/karabiner/
 
 cp -r ./ghostty/* ~/.config/ghostty/
 cp -r ./yazi/* ~/.config/yazi/
+cp -r ./sketchybar/* ~/.config/sketchybar/
+cp -r ./karabiner/* ~/.config/karabiner/
 
 cp .tmux.conf ~/
+cp .aerospace.toml
 
+# Apps
 brew install --cask docker
 brew install --cask ghostty
-brew install --cask firefox
 brew install --cask obsidian
 brew install --cask hot
+brew install --cask zen
+
+# sketchybar
+brew tap FelixKratz/formulae
+brew install sketchybar
+brew install --cask font-hack-nerd-font
+brew services start sketchybar
+
+#aerospace
+brew install --cask nikitabobko/tap/aerospace
 
 echo "Скрипт выполнен успешно!"
